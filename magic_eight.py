4a4d1dd4600e935331f20e9bca40d5ce1e09f763
Kevin Lee <kjunwonl@umich.edu>
2019-09-18 17:26:58 -0400

    new function added

--- magic_eight.py
+++ magic_eight.py
@@ -1,4 +1,13 @@
 def question():
     question_asked = input("What is your question: ")
     print(question_asked)
+    check_question(question_asked)
 
+def check_question(question):
+    if question.endswith('?'):
+        print("WOO THATS A QUESTION")
+    else:
+        while True:
+            new_input = input("I am sorry, I can only answer questions. ")
+            if new_input == "quit":
+                break
