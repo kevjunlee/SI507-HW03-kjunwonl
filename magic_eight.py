32d17f0cdd80dc9bb192d6a4d675eac637dfcfbb
Iain Graham <img@Iains-MacBook-Pro.local>
2019-09-19 00:01:45 -0400

    final push

--- magic_eight.py
+++ magic_eight.py
@@ -5,10 +5,7 @@ def question():
     print(question_asked)
     check_question(question_asked)
 
-
 answers = ['It is certain.', 'It is decidedly so.', 'Without a doubt', 'Yes - definitely.', 'You may rely on it', 'As i see it, yes.', 'Most likely', 'Outlookgood.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
-
-random.choice(answers)
 
 def check_question(question):
     if question.endswith('?'):
@@ -21,7 +18,5 @@ def check_question(question):
                 break
             elif new_input == "quit":
                 break
-
-
 
 question()
