401cf46f26b925b08b8fb47a3bfd937b15561ee3
Iain Graham <img@Iains-MacBook-Pro.local>
2019-09-18 20:15:22 -0400

    final program

--- magic_eight.py
+++ magic_eight.py
@@ -6,15 +6,22 @@ def question():
     check_question(question_asked)
 
 
-answers = ['It is certain.', 'It is decidedly so.', 'Without a doubt', 'Yes - definitely.', 'You may rely on it', 'As i see it, yes.', 'Most likey', 'Outlookgood.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
+answers = ['It is certain.', 'It is decidedly so.', 'Without a doubt', 'Yes - definitely.', 'You may rely on it', 'As i see it, yes.', 'Most likely', 'Outlookgood.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again.', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.']
 
-random_answer = random.choice(answers)
+random.choice(answers)
 
 def check_question(question):
     if question.endswith('?'):
-        print("WOO THATS A QUESTION")
+        print(random.choice(answers))
     else:
         while True:
-            new_input = input("I am sorry, I can only answer questions. ")
-            if new_input == "quit":
+            new_input = input("I am sorry, I can only answer questions. What is your question? ")
+            if new_input.endswith('?'):
+                print(random.choice(answers))
                 break
+            elif new_input == "quit":
+                break
+
+
+
+question()
